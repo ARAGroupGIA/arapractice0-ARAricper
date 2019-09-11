c243d102810efd9731842ca7105003d7d6d62013
ARAricper <55189127+ARAricper@users.noreply.github.com>
2019-09-11 14:22:06 +0200

    v1.0-int

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,5 +1,20 @@
 /* Here, you should entry the required libraries */
 
+#include <stdio.h>
+
 void main(){
 	/* Here, you should write the source code of your program */
+	
+	int a=0;
+	int b=0;
+	int sum=0;
+	
+	printf("Sum two numbers.\nFirst number please.\n");
+	scanf("%d",&a);
+	printf("Second number please.\n");
+	scanf("%d",&b);
+	
+	sum=(a+b);
+	
+	printf("Sum: %d",sum);
 }
