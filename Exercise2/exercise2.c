4d4340759bc94db27067c7c7e1413804afdef7cb
ARAricper <55189127+ARAricper@users.noreply.github.com>
2019-09-13 15:00:18 +0200

    v2.0

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -2,16 +2,18 @@
 
 #include <stdio.h>
 
-void main(){
+void main()
+{
 	/* Here, you should write the source code of your program */
-	
-	float a,b,sum;
+	float a, b, c, sum;
 	printf("Sum two numbers.\nFirst number please.\n");
-	scanf("%f",&a);
+	scanf("%f", &a);
 	printf("Second number please.\n");
-	scanf("%f",&b);
-	
-	sum=a+b;
-	
-	printf("Sum: %f",sum);
+	scanf("%f", &b);
+	printf("Third number please.\n");
+	scanf("%f", &c);
+
+	sum = a + b + c;
+
+	printf("Sum: %f", sum);
 }
